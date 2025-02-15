4073009681db929df2f2e3a90620b89d066e9e90
MINuxboy <minulapriyashan2004@gmail.com>
2025-02-15 15:43:04 +0530

    asesst bundaling

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -2,3 +2,13 @@ import axios from 'axios';
 window.axios = axios;
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+
+import toastr from 'toastr';
+toastr.options.toastClass="toastr";
+window.toastr = toastr;
+
+import Swal from 'sweetalert2'
+window.Swal = Swal; 
+
+import * as bootstrap from 'bootstrap'
+window.bootstrap = bootstrap;
